b3c7f51689878c33dac1f67f0cd195ba1f55a1f9
Hazem Omran <ha1452003@gmail.com>
2024-04-17 20:37:11 +0200

    adding-comments

--- Tilt_Game/Tilt_Game/Program.cs
+++ Tilt_Game/Tilt_Game/Program.cs
@@ -15,21 +15,27 @@ public class Tilt_Game
     
     public input read_test_file(string filename)
     {
+        //Read file
         string directoryPath = "Sample Tests";
         string filePath = Path.Combine(directoryPath, filename);
-
         FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-
         StreamReader sr = new StreamReader(file);
-        string line = sr.ReadLine();
+
         input values;
+
+        //Read board dimention (n)
+        string line = sr.ReadLine();
         values.n = int.Parse(line);
+
+        //Read board (B)
         values.B = new char[values.n][];
         for(int i = 0; i <  values.n; i++)
         {
             line = sr.ReadLine();
             values.B[i] = line.Where(c => !char.IsWhiteSpace(c)).ToArray();
         }
+
+        //Read target dimentions (x, y)
         line = sr.ReadLine();
         string[] target = line.Split(", ");
         values.x = int.Parse(target[0]);
@@ -49,10 +55,7 @@ class Program
     {
         Tilt_Game game = new Tilt_Game();
         input values = game.read_test_file("Case1.txt");
-        Console.WriteLine(values.n);
-        Console.WriteLine(values.x); Console.WriteLine(values.y);
         
-
     }
 
 
